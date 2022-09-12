3a6e76f6a0e27cd07900d635621cc90de2be25b1
JonathinK <jonathin.kish@gmail.com>
2022-09-12 09:17:27 -0400

    changed hero

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -10,7 +10,6 @@ const HeroImage = () =>{
  
   return(
     <HeroGrid>
-      <HeroWrap>
         <HeroBackground>
           <StaticImage
             src="../images/Background.jpg"
@@ -20,7 +19,6 @@ const HeroImage = () =>{
             layout="fullWidth"
           />
         </HeroBackground>
-      </HeroWrap>
       <HeroOverlay />
       <HeroInfo>
         <h1>One Goal</h1>
@@ -40,33 +38,38 @@ const HeroGrid = styled.div`
   min-height:100vh;
   height:100vh;
 `
-
-const HeroWrap = styled.div`
-  grid-column: 1 / 2;
+const HeroBackground = styled.div`
   grid-row: 1 / 2;
+  grid-column: 1 / 2;
   height:100vh;
   min-height:100vh;
-`
-const HeroBackground = styled.div`
-  grid-area: 1 / 1 / 2 / 2;
   
   .heroBackground{
     position:absolute;
     height:100%;
     width:100%;
+    top:0;
+    bottom:0;
+    right:0;
+    left:0;
   }
 `
 const HeroOverlay = styled.div`
-  grid-area: 1 / 1 / 2 / 2;
+  grid-row: 1 / 2;
+  grid-column: 1 / 2;
   width:100%;
   height:100%;
+  min-height:100vh;
   background:rgba(0,0,0, .4);
   position:relative;
 `
 const HeroInfo = styled.div`
-  grid-area: 1 / 1 / 2 / 2;
+  grid-row: 1 / 2;
+  grid-column: 1 / 2;
+  justify-self:center;
+  align-self:center;
   width:100%;
-  height:100%;
+  height:fit-content;
   display:flex;
   flex-flow:column;
   justify-content:center;
@@ -90,7 +93,7 @@ const HeroInfo = styled.div`
   }
   p{
     font-size:clamp(1.5rem, 3vw, 5rem);
-    margin: 4rem 1rem 0 1rem;
+    margin: 1rem 1rem 0 1rem;
     color:white;
     text-align:center;
     letter-spacing:3px;
@@ -98,12 +101,12 @@ const HeroInfo = styled.div`
 
   @media ${breakpoints.md}{
       justify-content:flex-start;
-      margin-top: 15rem;
+      
   }
 
   @media ${breakpoints.sm}{
       justify-content:flex-start;
-      margin-top:23vh;
+      margin-top:25vh;
 
       h1{
         :first-child{
