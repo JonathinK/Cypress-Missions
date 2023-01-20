67af8d241ff99b4df69c82c735fec958b2a16d1d
JonathinK <jonathin.kish@gmail.com>
2023-01-20 14:37:45 -0500

    changed token

--- gatsby-config.js
+++ gatsby-config.js
@@ -48,7 +48,7 @@ module.exports = {
       resolve:`gatsby-source-contentful`,
       options: {
         spaceId: process.env.CONTENTFUL_SPACE_ID || '',
-        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN || '',
+        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
         host: process.env.CONTENTFUL_HOST || 'cdn.contentful.com',
       }   
     }   
